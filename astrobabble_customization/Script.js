fcf16b145dafe848c556aff2d1d7a834ef812a14
Spacecow90 <nabmcmullin@gmail.com>
2022-03-26 17:46:12 -0400

    Small changes to Customization page
    Changed some variable naming and made it so the character you play as is where it loads to when you return to the customization screen.

--- astrobabble_customization/Script.js
+++ astrobabble_customization/Script.js
@@ -1,35 +1,58 @@
-let colour = "Basic"
-let avatar = "Default"
+let colourchoice
+let avatar
 let characterchoice
-let avatarchoice = 0
+let avatarchoice
 let awesome
 // tells it to run the charfinal function upon loading the page which gets it to display the basic character right away
 window.onload = function (begin){
+    //this section loads in the previous choice of avatar and colour so that it's the same as where you left off, unless you haven't picked before in which case, it sets the values to their defaults
+
+    colourchoice = localStorage.getItem("colourchoice")
+    avatarchoice = localStorage.getItem("avatarchoice")
+    //if you've never played sets values to default
+    if (colourchoice == undefined){
+        colourchoice = "Basic"
+    }
+    //if the variables called do have info, meaning you've played before, do this:
+    else {
+        colourchoice = localStorage.getItem("colourchoice")
+    }
+     //if you've never played sets values to default
+    if (avatarchoice == null){
+    avatarchoice = 0
+    }
+    //if the variables called do have info, meaning you've played before, do this:
+    else {
+    avatarchoice = localStorage.getItem("avatarchoice")
+    }
+    //load the charfinal function
     CharFinal();
 }
 //the right arrow button tells the avatar choice variable to go up by one
 function Right (){
 avatarchoice++
+colourchoice = "Basic"
 }
 //the left arrow button tells the avatar choice variable to go down by one
 function Left (){
 avatarchoice--
+colourchoice = "Basic"
 }
 //this function activates when the basic colour button is clicked
 function Basic(){
-    colour = "Basic"
+    colourchoice = "Basic"
 }
 //this function activates when the first alt colour button is clicked
 function Alt1(){
-    colour = "Alt1"
+    colourchoice = "Alt1"
 } 
 //this function activates when the second alt colour button is clicked
 function Alt2(){
-    colour = "Alt2"
+    colourchoice = "Alt2"
 } 
 //this function activates when the third alt colour button is clicked note: it's only available for the default avatar and is disabled until you reach level 10 atleast once.
 function Alt3(){
-    colour = "Alt3"
+    colourchoice = "Alt3"
 } 
 //this function takes the avatarchoice and colour variables from above to decide the look and image displayed on the page
 function CharFinal(){
@@ -84,7 +107,7 @@ function CharFinal(){
         document.getElementById("alt3").style.opacity = "0%"
     }
     // this combines all the above variables to create the image file location so that it can load the correct images. 
-    characterchoice = "Images/" + avatar + colour +".png"
+    characterchoice = "Images/" + avatar + colourchoice +".png"
     //this sets the image source to the above mentioned variable
     document.getElementById("avatarimage").src = characterchoice
     //can be removed, good for testing purposes
@@ -92,5 +115,8 @@ function CharFinal(){
     console.log (avatarchoice)
     //this transfers the variable of the character choice from this page to any other that wants to use it. 
     localStorage.setItem("characterchoice", characterchoice);
+    localStorage.setItem("avatarchoice", avatarchoice);
+    localStorage.setItem("colourchoice", colourchoice);
+
 
 }
