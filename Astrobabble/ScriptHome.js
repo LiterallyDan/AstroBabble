476979e6c93575504a76da8b50a95c94df7fe5e1
LiterallyDan <hockey4life1233@gmail.com>
2022-03-26 15:28:14 -0400

    Create ScriptHome.js

---
+++ Astrobabble/ScriptHome.js
@@ -0,0 +1,15 @@
+/* ----------------------------------
+           Home Page JS
+------------------------------------*/
+
+//Changes setting cog to darker color
+function changeSettings()
+{
+    document.getElementById("myImage").src = "Images/settingsCog2.png";
+}
+
+//Resets settings cog to original color
+function resetSettings()
+{
+    document.getElementById("myImage").src = "Images/settingsCog.png";
+}
